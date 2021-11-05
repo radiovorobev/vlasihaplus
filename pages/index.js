ad39617818e201ad70d6cdc21fea35e989e4cf56
Sergey Vorobev <radio.vorobev@yandex.ru>
2021-11-06 01:36:29 +0300

    nav & animation

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,32 @@
-const websiteSubtitles = [
-	{ text: 'Новости', color: '#00ff19' },
-	{ text:	'Эфир', color: '#00ffff' },
-	{ text: 'Люди', color: '#2764ff' },
-	{ text: 'Спорт', color: '#ff0000' },
-	{ text: 'Art', color: '#fa00ff' }
-];
-const websiteSubtitle = document.querySelector('.website-title__subtitle');
+function changeSubtitle() {
+	const websiteSubtitles = [
+		{ text: 'Новости', color: '#00ff19' },
+		{ text:	'Эфир', color: '#00ffff' },
+		{ text: 'Люди', color: '#2764ff' },
+		{ text: 'Спорт', color: '#ff0000' },
+		{ text: 'Art', color: '#fa00ff' }
+	];
+	const websiteSubtitle = document.querySelector('.website-title__subtitle');
 
-function getRandomElement(arr) {
-	let randIndex = Math.floor(Math.random() * arr.length);
-	return arr[randIndex];
+	let i = 0;
+	let timer = setInterval(function () {
+		if (i == websiteSubtitles.length - 1) {
+			i = 0;
+			websiteSubtitle.textContent = websiteSubtitles[i].text;
+			websiteSubtitle.style.webkitTextStrokeColor = websiteSubtitles[i].color;
+		} else {
+			i++;
+			websiteSubtitle.textContent = websiteSubtitles[i].text;
+			websiteSubtitle.style.webkitTextStrokeColor = websiteSubtitles[i].color;
+		}
+	}, 3000);
 }
 
-setInterval(function() {
-	let randomElement = getRandomElement(websiteSubtitles);
-	websiteSubtitle.textContent = randomElement.text;
-	websiteSubtitle.style.webkitTextStrokeColor = randomElement.color;
-		}, 3000);
+changeSubtitle();
+const plus = document.querySelector('.plus');
+plus.addEventListener('animationend', () => {
+	plus.classList.remove('rotation');
+	plus.offsetWidth;
+	plus.classList.add('rotation');
 
-
-anime({
-	targets: '.website-title .website-title__subtitle',
-	translateY: -100,
-	loop: true,
-	delay: 2500,
-	easing: 'linear',
-	duration: 500,
 });
